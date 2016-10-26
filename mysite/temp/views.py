bb9549cad02671372e7ca02b00b51ecd6db8dd4c
David Ranshous <ranshous93@Davids-MacBook-Pro.local>
2016-10-26 15:17:27 -0400

    forked repository

--- mysite/temp/views.py
+++ mysite/temp/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+import json, requests
+
 from .models import Sensor, Feed
+
+# where all the stuff for the web app should be located
 
 def index(request):
     sensor_list = Sensor.objects.order_by('-sensor_name')
@@ -10,4 +14,30 @@ def index(request):
     context = {
         'sensor_list': sensor_list,
     }
+
+    
+    #prepare dictionary
+    auth = {"email": "chris.carper@atelierten.com", "password": "A10green"}
+
+    #convert dictionary to json
+    json_data = json.dumps(auth)
+
+    #make post request for authorization
+    #if successful, req will be authorization token
+    req = requests.post('https://cloud.kierantimberlake.com/pointelist/api/api-token-auth/',
+            data=json_data,
+            headers={'content-type': 'application/json'})
+
+    #convert from json to dictionary
+    json_load = req.json()
+
+    #make request for data
+    #this specific request would take the last ten results from chart 93
+    req = requests.get('https://cloud.kierantimberlake.com/pointelist/api/samples?chart=93&last=10',
+            headers={'content-type': 'application/json', 'cookie': "token=" + json_load['token']})
+
+    #convert from json
+    #load = req.json()
+    
+
     return HttpResponse(template.render(context, request))
